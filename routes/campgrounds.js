c4247e066e7f837ee3d9b9fd5c935db5b5aca90e
erixerns <erixerns@gmail.com>
2018-06-30 14:31:06 +0530

    Added user associations for campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -26,11 +26,13 @@ router.get("/",function(req,res){
   });
 });
 
-router.post("/",function(req,res){
+router.post("/",isLoggedIn, function(req,res){
   var name=req.body.name;
   var image=req.body.image;
 
-  var newCamp = {name:name, image:image};
+  var author = {id: req.user._id, username: req.user.username};
+  var newCamp = {name:name, image:image, author:author};
+  
   Campground.create(newCamp,function(err,campground){
     if(err)
       console.log(err);
@@ -39,7 +41,7 @@ router.post("/",function(req,res){
   });
 });
 
-router.get("/new",function(req,res){
+router.get("/new",isLoggedIn, function(req,res){
   res.render("campgrounds/new");
 });
 
